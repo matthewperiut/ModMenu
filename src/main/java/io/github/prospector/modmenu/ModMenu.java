5e7616b58fce1a50b61a0db114d47d3e80c09712
telontelvarost <telontelvarost@gmail.com>
2024-05-03 16:37:16 -0400

    ISSUE-0 - Do real fix for mods within mods as libraries; Count java as a library

--- src/main/java/io/github/prospector/modmenu/ModMenu.java
+++ src/main/java/io/github/prospector/modmenu/ModMenu.java
@@ -75,10 +75,15 @@ public class ModMenu implements ClientModInitializer {
 		for (ModContainer mod : mods) {
 			ModMetadata metadata = mod.getMetadata();
 			String id = metadata.getId();
+
 			if (metadata.containsCustomValue("modmenu:api") && metadata.getCustomValue("modmenu:api").getAsBoolean()) {
 				addLibraryMod(id);
-			} else if (metadata.getAuthors().isEmpty()) {
+			}
+			if (!id.equals("minecraft") && mod.getMetadata().getType().equals("builtin")) {
 				addLibraryMod(id);
+			}
+			for (ModContainer containedMod : mod.getContainedMods()) {
+				addLibraryMod(containedMod.getMetadata().getId());
 			}
 			if (metadata.containsCustomValue("modmenu:clientsideOnly") && metadata.getCustomValue("modmenu:clientsideOnly").getAsBoolean()) {
 				CLIENTSIDE_MODS.add(id);
