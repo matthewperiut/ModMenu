1ada628cb2dbcb79a574e39714b40b5b61bbb899
telontelvarost <telontelvarost@gmail.com>
2024-05-02 20:34:42 -0400

    ISSUE-0 - Add texturepack button to pause menu

--- src/main/java/io/github/prospector/modmenu/mixin/MixinGameMenuScreen.java
+++ src/main/java/io/github/prospector/modmenu/mixin/MixinGameMenuScreen.java
@@ -5,6 +5,7 @@ import io.github.prospector.modmenu.gui.ModListScreen;
 import io.github.prospector.modmenu.gui.ModMenuButtonWidget;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.pack.PackScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,12 +18,16 @@ public class MixinGameMenuScreen extends Screen {
 	@SuppressWarnings("unchecked")
 	@Inject(at = @At("RETURN"), method = "init")
 	public void drawMenuButton(CallbackInfo info) {
-		this.buttons.add(new ModMenuButtonWidget(100, this.width / 2 - 100, this.height / 4 + 72 - 16, 200, 20,  "Mods (" + ModMenu.getFormattedModCount() + " loaded)"));
+		this.buttons.add(new ButtonWidget(100, this.width / 2 - 100, this.height / 4 + 72 - 16, 98, 20, "Texture Packs"));
+		this.buttons.add(new ModMenuButtonWidget(101, this.width / 2 + 2, this.height / 4 + 72 - 16, 98, 20, "Mods (" + ModMenu.getFormattedModCount() + " loaded)"));
 	}
 
 	@Inject(method = "buttonClicked", at = @At("HEAD"))
 	private void onActionPerformed(ButtonWidget button, CallbackInfo ci) {
 		if (button.id == 100) {
+			minecraft.setScreen(new PackScreen(this));
+		}
+		if (button.id == 101) {
 			minecraft.setScreen(new ModListScreen(this));
 		}
 	}
